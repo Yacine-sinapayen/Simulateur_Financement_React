9dcb742e664a4a61bd300a32039217701daf9883
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-05-02 11:10:47 +0200

    test-1 preprod responsive

--- src/Components/Pages/Etudiant.js
+++ src/Components/Pages/Etudiant.js
@@ -2,30 +2,41 @@ import React from 'react'
 import LeftPart from '../LeftPart/LeftPart';
 import { motion } from 'framer-motion'
 
+import useDimension from '../hook/useDimension';
+
+
 export default function Etudiant() {
 
   const learnyLib = () => {
     window.open('https://www.learnylib.com/', '_blank');
   }
-  return (
-    <>
-      <div className='flex'>
-        <LeftPart />
 
-        <motion.div
-          className='w100p h100vh center-content flex column'
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-        >
-          <h1 className='text-center'>Vous êtes Étudiant.e</h1>
-          <div className='flex wrap w75p center-content'>
+  const browserWidth = useDimension();
+
+  return (
+    <div className={browserWidth < 700 ? 'flex column' : 'flex row'}>
+
+      <LeftPart />
+
+      <motion.div
+        className='right-part-responsive w100p h100vh flex column center-content'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+
+        <div className='center-content flex column'>
+
+          <h1 className='text-center mrg-10'>Vous êtes Étudiant.e</h1>
+
+          <div className='flex wrap center-content w75p '>
             <p className='text-center mrg-b10'>
               Etudiant.e : pas de prise en charge, vous pouvez bénéficier d’une remise étudiant au moment de votre inscription.
             </p>
-            <button className="center-content" onClick={learnyLib}>Choisir ma formation</button>
+            <button className="btn center-content mrg-10" onClick={learnyLib}>Choisir ma formation</button>
+
           </div>
-        </motion.div>
-      </div>
-    </>
+        </div>
+      </motion.div>
+    </div>
   )
 }
